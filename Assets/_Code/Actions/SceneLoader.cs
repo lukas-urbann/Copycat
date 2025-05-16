db2b099dd8f46aff244ed50b31bc2df7a6485896
Lukáš Urban <lukas.urbann@seznam.cz>
2025-05-16 15:29:07 +0200

    bordel

---
+++ Assets/_Code/Actions/SceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BachelorProject.Actions
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public Scene asd;
+    }
+}
