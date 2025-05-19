8c3271961438e02c86cc733a7b44118fcfb8934e
Lukáš Urban <lukas.urbann@seznam.cz>
2025-05-19 22:40:06 +0200

    kurva jestli to budu fixovat jeste jednou tak neco rozbiju

--- Assets/_Code/Events/Event/BoolEvent.cs
+++ Assets/_Code/Events/Event/BoolEvent.cs
@@ -5,20 +5,24 @@ namespace BachelorProject.Events
     [CreateAssetMenu(menuName = "Events/Bool Event")]
     public class BoolEvent : ParameterEvent<bool>
     {
+        [SerializeField] private bool executeWhenFalse = true;
         [SerializeField] private BoolReference boolReference;
         
         public void Execute(bool value)
         {
+            if (!executeWhenFalse && !value) return;
             base.Execute(value);
         }
         
         public void Execute(BoolReference reference)
         {
+            if (!executeWhenFalse && !reference.Value) return;
             base.Execute(reference.Value);
         }
 
         public void Execute()
         {
+            if (!executeWhenFalse && !boolReference.Value) return;
             base.Execute(boolReference.Value);
         }
     }
